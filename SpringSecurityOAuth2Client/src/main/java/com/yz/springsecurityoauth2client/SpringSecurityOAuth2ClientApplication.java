6420b6ea75d1d0ba36753a53ddfd6f8291d9754e
azir <794070011@qq.com>
2022-02-25 17:52:46 +0800

    修改认证模块

--- SpringSecurityOAuth2Client/src/main/java/com/yz/springsecurityoauth2client/SpringSecurityOAuth2ClientApplication.java
+++ SpringSecurityOAuth2Client/src/main/java/com/yz/springsecurityoauth2client/SpringSecurityOAuth2ClientApplication.java
@@ -2,8 +2,10 @@ package com.yz.springsecurityoauth2client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringSecurityOAuth2ClientApplication {
 
     public static void main(String[] args) {
---
+++ SpringSecurityOAuth2Server/src/main/java/com/yz/springsecurityoauth2server/config/Utils.java
@@ -0,0 +1,13 @@
+package com.yz.springsecurityoauth2server.config;
+
+/**
+ * @author yanzhao
+ * @version 1.0
+ * TODO
+ * @date 2022/2/25 17:03
+ */
+public class Utils {
+    public static class RESOURCEIDS {
+        static final String ORDER = "order";
+    }
+}
