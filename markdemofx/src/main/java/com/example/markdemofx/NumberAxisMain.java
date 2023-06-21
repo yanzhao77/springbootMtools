768347ef726c4094da4e000c5d3766b4e0ebf327
azir <756624050@qq.com>
2023-06-21 17:54:25 +0800

    修改版本对应

---
+++ markdemofx/src/main/java/com/example/markdemofx/NumberAxisMain.java
@@ -0,0 +1,54 @@
+package com.example.markdemofx;
+
+/**
+ * @author yanzhao
+ * @version 1.0
+ * @classname asd
+ * @date 2023/05/12 13:47
+ * @description TODO
+ */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class NumberAxisMain extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("X Axis");
+        yAxis.setLabel("Y Axis");
+
+        //creating the chart
+        final LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Sin 45");
+
+        //populating the series with data
+        for (int i = 0; i <= 500; i++) {
+            double x = Math.toRadians(i);
+            double y = Math.sin(x) * Math.sqrt(2) / 2;
+            series.getData().add(new XYChart.Data(x, y));
+        }
+
+        //adding the series to the chart
+        lineChart.getData().add(series);
+
+        //setting the scene
+        Scene scene = new Scene(lineChart, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
